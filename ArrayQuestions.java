aaca56912799a7b397391dfc38520291267d26af
tj-recess <tj.recess@live.com>
2013-11-27 02:16:40 +0530

    Count occurrences of given integer in a sorted array in O (log n) time

--- ArrayQuestions.java
+++ ArrayQuestions.java
@@ -6,20 +6,82 @@ public class ArrayQuestions {
         ArrayQuestions aq = new ArrayQuestions();
 
         int size = Integer.parseInt(args[0]);
-        int[] arr = aq.populateArray(size);
-        List<Integer> result = aq.getContiguousLargestSum(arr);
-        System.out.println("Largest contiguous sum in array " + Arrays.toString(arr) + " is: " + result.get(2) + "; startIndex = " + result.get(0) + "; endIndex = " + result.get(1));
-        System.out.println("Max sum with no two contiguous elements in array " + Arrays.toString(arr) + " is: " + aq.maxSumNoTwoContiguous(arr));
+        int[] arr = aq.populateArray(size, false, 5);
+        Arrays.sort(arr);
+        System.out.println("Array: " + Arrays.toString(arr));
+        int x = Integer.parseInt(args[1]);
+        System.out.println(x + " occurs " + aq.countOccurrences(arr, x) + " times.");
+        // List<Integer> result = aq.getContiguousLargestSum(arr);
+        // System.out.println("Largest contiguous sum in array " + Arrays.toString(arr) + 
+            // " is: " + result.get(2) + "; startIndex = " + result.get(0) + "; endIndex = " + result.get(1));
+        // System.out.println("Max sum with no two contiguous elements in array " + Arrays.toString(arr) + 
+            // " is: " + aq.maxSumNoTwoContiguous(arr));
+
     }
 
-    private int[] populateArray(int size) {
+    /**
+    * Finds occurences of given integer in a sorted array in O(log n) time
+    */
+    public int countOccurrences(int[] arr, int x) {
+        // perform binary search to get first location of x,
+        // perform binary search to get last location of x
+        // return last - first + 1;
+        int first = binarySearchFirst(arr, x);
+        int last = binarySearchLast(arr, x);
+        return first == -1 ? -1 : (last - first + 1);
+    }
+
+    private int binarySearchLast(int[] arr, int x) {
+        int start = 0;
+        int end = arr.length-1;
+
+        while (start <= end) {
+            int mid = start + (end-start)/2;
+            if (arr[mid] == x && (mid == arr.length-1 || arr[mid+1] > x)) {
+                return mid;
+            } 
+            // go right
+            else if (arr[mid] < x || arr[mid+1] == x) {
+                start = mid+1;
+            }
+            // go left
+            else {
+                end = mid-1;
+            }
+        }
+        // not found
+        return -1;
+    }
+
+    private int binarySearchFirst(int[] arr, int x) {
+        int start = 0;
+        int end = arr.length-1;
+
+        while (start <= end) {
+            int mid = start + (end-start)/2;
+            if (arr[mid] == x && (mid == 0 || arr[mid-1] < x)) {
+                return mid;
+            } 
+            // go left
+            else if (arr[mid] > x || arr[mid-1] == x) {
+                end = mid-1;
+            }
+            // go right
+            else {
+                start = mid+1;
+            }
+        }
+        // not found
+        return -1;
+    }
+
+    private int[] populateArray(int size, boolean allowNegative, int maxValue) {
         Random r = new Random();
         int[] arr = new int[size];
         for (int i  = 0; i < size; i++) {
-            if (r.nextBoolean()) {
-                arr[i] = r.nextInt(11);
-            } else {
-                arr[i] = -1 * r.nextInt(11);
+            arr[i] = r.nextInt(maxValue);
+            if (allowNegative && r.nextBoolean()) {
+                arr[i] = -1 * r.nextInt(maxValue);
             }
         }
         return arr;
